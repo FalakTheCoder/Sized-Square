3ac72220c09a6211f9673c2280d30fff4eef038a
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-03-17 17:25:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+diifference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('poses', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+
+function draw(){
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "Width and Height of the square will be = " + diifference + "px";
+    Fill('#F90093');
+    stroke('F90090');
+    square(noseX, noseY, diifference);
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = "+noseX+"Nose Y = "+noseY );
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        diifference = floor(leftWristX - rightWristX);
+    }
+}
